59f594ef02d27bdfbca417db8a1d79fa5236a18f
Richard Fehling <richard.fehling.mte21lin@tucsweden.se>
2021-02-08 14:10:12 +0100

    Test av Git

--- src/main/java/tuc/hello_world/Main.java
+++ src/main/java/tuc/hello_world/Main.java
@@ -5,6 +5,7 @@ package tuc.hello_world;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Second try");
     }
     
 }
